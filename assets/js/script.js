9a9dadea4dc71563c9d0f2ee6e48f4100185fdca
savinaxu <jia_shijie@163.com>
2018-11-03 13:52:12 -0700

    result page testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,9 @@ $(function() {
     var travelClientSecret = "3W01MIAU01QTD0L4W3QE3NI4PQYZB1WNHMGQZ4UYGQVWYTJI";
     //travel Data
     var locationIds;
-    
+    var locationNames;
+    var locationDescriptions;
+
 
 //result page header
 //==========================================================================
@@ -27,9 +29,32 @@ $(function() {
 //result page landmarks
 //==========================================================================
 
-    function renderLandmarks() {
+    function renderLandmarks(landmarkImgUrl, landmarkName, landmarkDescription) {
+        //wrap div
+        var landmarkDiv = $("<div class='landmarks-individual-content-container'>")
 
+        //content div
+        var landmarkContentContainer = $("<div class='individual-content-container'>")
+        var landmarkImg = $("<img class='individual-content-img'>")
+        var individualContentContainer = $("<div class='individual-content'>")
+        var individualContentHeader = $("<h1 class='individual-content-header'>")
+        var individualContentDescription = $("<p class='individual-content-description'>")
+
+        //dividing div
+        var dividing = $("<div class='individual-content-dividing-line'>")
+
+        //insert data
+        $(".individual-content-img").attr("src", landmarkImgUrl)
+        $(".individual-content-header").text(landmarkName)
+        $(".individual-content-description")
+
+        //append to html page
+        $(".individual-content").append(individualContentHeader, individualContentDescription)
+        $(".individual-content-container").append(landmarkImg, individualContentContainer)
+        $(".landmarks-content-container").append(landmarkContentContainer, dividing)
     }
+
+//==========================================================================
 
 
 
